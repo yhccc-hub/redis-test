7cc2beb40d2dcbc2e31861edbe5ad2e099ad9895
Henson <Henson@ouc.edu.cn>
2021-05-29 15:59:36 +0800

    first commit redis-git

---
+++ src/main/java/com.yhc/JedisKeyTest.java
@@ -0,0 +1,12 @@
+import redis.clients.jedis.Jedis;
+
+public class JedisKeyTest {
+    public static void main(String[] args) {
+        //连接redis
+        Jedis jedis = new Jedis("192.168.44.128",6379);
+        //
+        String ping = jedis.ping();
+        System.out.println(ping);
+        jedis.keys("*");
+    }
+}
