6c1e778a7f68f5867eff6f21d00ee4e3a8c2fc7a
Henson <Henson@ouc.edu.cn>
2021-05-30 10:44:34 +0800

    push test

--- src/main/java/com.yhc/JedisKeyTest.java
+++ src/main/java/com.yhc/JedisKeyTest.java
@@ -11,5 +11,6 @@ public class JedisKeyTest {
         System.out.println("hello");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
